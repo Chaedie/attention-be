c0e98f55ab90db9da0e0c08d1178b3e8d1085a0d
ChaedongIm <chaedong.im.dev@gmail.com>
2023-03-09 16:37:18 +0900

    Add: 레디스 클라우드 연결 성공

--- config/express.js
+++ config/express.js
@@ -32,23 +32,18 @@ if (process.env.NODE_ENV === "production") {
 } else {
   app.use(morgan("dev"));
 }
-
-// Redis Setting
-// const redis = require("redis");
-
-// const RedisStore = require("connect-redis")(session);
-
-// const redisClient = redis.createClient({
-//   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
-//   legacyMode: true, // 반드시 설정 !!
-// });
-// redisClient.on("connect", () => {
-//   logger("info", "Redis Connected!");
-// });
-// redisClient.on("error", err => logger.error("Redis Client Error"));
-// redisClient.connect().then();
-// const redisCli = redisClient.v4;
-// const redisStore = new RedisStore({ client: redisClient });
+// Redis 설정
+const Redis = require("redis");
+const { errorHandlerWrapper } = require("../src/middlewares/errorhandler");
+const redisClient = new Redis.createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+  legacyMode: true,
+});
+redisClient.connect().then().catch(console.error);
 
 //
 app.use(express.json());
@@ -73,4 +68,16 @@ if (process.env.NODE_ENV === "production") {
 }
 app.use(session(sessionOption));
 
+async function redisCheckFunction(req, res) {
+  redisClient.get("test", async (err, data) => {
+    if (err) console.error(err);
+    if (data) {
+      res.json(data);
+    } else {
+      redisClient.set("test", "goooooooood!!!!");
+    }
+  });
+}
+
+app.get("/redis-check", errorHandlerWrapper(redisCheckFunction));
 module.exports = app;
