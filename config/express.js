25fe36136f6882136914305a868412e4b04220d9
ChaedongIm <chaedong.im.dev@gmail.com>
2023-03-09 15:40:33 +0900

    Fix: 레디스 설정 해제

--- config/express.js
+++ config/express.js
@@ -18,17 +18,6 @@ app.use(
   }),
 );
 
-const redis = require("redis");
-const RedisStore = require("connect-redis").default;
-
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
-  legacyMode: true,
-});
-redisClient.connect().catch(err => logger.error("redis Error입니다." + err));
-
 if (process.env.NODE_ENV === "production") {
   app.enable("trust proxy");
   app.use(morgan("combined"));
@@ -44,19 +33,38 @@ if (process.env.NODE_ENV === "production") {
   app.use(morgan("dev"));
 }
 
+// Redis Setting
+// const redis = require("redis");
+
+// const RedisStore = require("connect-redis")(session);
+
+// const redisClient = redis.createClient({
+//   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
+//   legacyMode: true, // 반드시 설정 !!
+// });
+// redisClient.on("connect", () => {
+//   logger("info", "Redis Connected!");
+// });
+// redisClient.on("error", err => logger.error("Redis Client Error"));
+// redisClient.connect().then();
+// const redisCli = redisClient.v4;
+// const redisStore = new RedisStore({ client: redisClient });
+
+//
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser(process.env.COOKIE_SECRET));
+//
+app.use(cookieParser(process.env.REDIS_PASSWORD));
 const sessionOption = {
   resave: false,
   saveUninitialized: true,
-  secret: process.env.COOKIE_SECRET,
+  secret: process.env.REDIS_PASSWORD,
   cookie: {
     httpOnly: true,
     secure: false,
   },
-  store: new RedisStore({ client: redisClient }),
+  // store: redisStore,
 };
 if (process.env.NODE_ENV === "production") {
   sessionOption.proxy = true; // nginx 사용할 떄 true가 필요하다.
